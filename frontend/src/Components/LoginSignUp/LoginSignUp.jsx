19908c05c18b22841f273ea1b0d53747e78fba24
sineth <dhananjayaaps@gmail.com>
2023-08-27 11:48:36 +0530

    frontend

---
+++ frontend/src/Components/LoginSignUp/LoginSignUp.jsx
@@ -0,0 +1,42 @@
+// import React from "react";
+// import './LoginSignUp.css'
+
+// import user_icon from '../Assets/user-icon.png'
+// import email_icon from '../Assets/email-icon.png'
+// import password_icon from '../Assets/password-icon.png'
+// import name_icon from '../Assets/name-icon.png'
+
+// import { useCallback } from "react";
+// import Particles from "react-tsparticles";
+// import { loadFull } from "tsparticles";
+
+// const LoginSigup = () => {
+//     return(
+//         <div className="container">
+//             <div className="header">
+//                 <div className="text">Sign Up </div>
+//                 <div className="underline"></div>
+//             </div>
+//             <div className="inputs">
+//                 <div className="input">
+//                     <img src={user_icon} alt="" />
+//                     <input type="text" placeholder="Name"/>
+//                 </div>
+//                 <div className="input">
+//                     <img src={email_icon} alt=""/>
+//                     <input type="email" placeholder="Email"/>
+//                 </div>
+//                 <div className="input">
+//                     <img src={password_icon} alt="" />
+//                     <input type="password" placeholder="Password"/>
+//                 </div>
+//             </div>
+//             <div className="forgot-password">Lost Password ? <span>Click Here</span></div>
+//             <div className="submit-container">
+//                 <div className="submit">Sign Up</div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default LoginSigup
