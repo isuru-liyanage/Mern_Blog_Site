3fc705506c0c038db3b1e060a8d540f83543c81d
sineth <dhananjayaaps@gmail.com>
2023-08-27 16:56:44 +0530

    Login And SignUp pages created

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,174 +1,38 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-import { ToastContainer, toast } from "react-toastify";
-import './Login.scss'
+import './Login.css'
 
-// const Login = () => {
-//   const navigate = useNavigate();
-//   const [inputValue, setInputValue] = useState({
-//     email: "",
-//     password: "",
-//   });
-//   const { email, password } = inputValue;
-//   const handleOnChange = (e) => {
-//     const { name, value } = e.target;
-//     setInputValue({
-//       ...inputValue,
-//       [name]: value,
-//     });
-//   };
-
-//   const handleError = (err) =>
-//     toast.error(err, {
-//       position: "bottom-left",
-//     });
-//   const handleSuccess = (msg) =>
-//     toast.success(msg, {
-//       position: "bottom-left",
-//     });
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const { data } = await axios.post(
-//         "http://localhost:4000/login",
-//         {
-//           ...inputValue,
-//         },
-//         { withCredentials: true }
-//       );
-//       console.log(data);
-//       const { success, message } = data;
-//       if (success) {
-//         handleSuccess(message);
-//         setTimeout(() => {
-//           navigate("/");
-//         }, 1000);
-//       } else {
-//         handleError(message);
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//     setInputValue({
-//       ...inputValue,
-//       email: "",
-//       password: "",
-//     });
-//   };
-
-//   return (
-//     <div className="form_container">
-//       <h2>Login Account</h2>
-//       <form onSubmit={handleSubmit}>
-//         <div>
-//           <label htmlFor="email">Email</label>
-//           <input
-//             type="email"
-//             name="email"
-//             value={email}
-//             placeholder="Enter your email"
-//             onChange={handleOnChange}
-//           />
-//         </div>
-//         <div>
-//           <label htmlFor="password">Password</label>
-//           <input
-//             type="password"
-//             name="password"
-//             value={password}
-//             placeholder="Enter your password"
-//             onChange={handleOnChange}
-//           />
-//         </div>
-//         <button type="submit">Submit</button>
-//         <span>
-//           Already have an account? <Link to={"/signup"}>Signup</Link>
-//         </span>
-//       </form>
-//       <ToastContainer />
-//     </div>
-//   );
-// };
-
-// export default Login;
-
-
-const mode = 'login';
-
-class LoginComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            mode: this.props.mode
-        }
-    }
-    toggleMode() {
-        var newMode = this.state.mode === 'login' ? 'signup' : 'login';
-        this.setState({ mode: newMode});
-    }
-    render() {
-        return (
-            <div>
-                <div className={`form-block-wrapper form-block-wrapper--is-${this.state.mode}`} ></div>
-                <section className={`form-block form-block--is-${this.state.mode}`}>
-                    <header className="form-block__header">
-                        <h1>{this.state.mode === 'login' ? 'Welcome back!' : 'Sign up'}</h1>
-                        <div className="form-block__toggle-block">
-                            <span>{this.state.mode === 'login' ? 'Don\'t' : 'Already'} have an account? Click here &#8594;</span>
-                            <input id="form-toggler" type="checkbox" onClick={this.toggleMode.bind(this)} />
-                            <label htmlFor="form-toggler"></label>
-                        </div>
-                    </header>
-                    <LoginForm mode={this.state.mode} onSubmit={this.props.onSubmit} />
-                </section>
-            </div>
-        )
-    }
-}
-
-
-class LoginForm extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-        <form onSubmit={this.props.onSubmit}>
-            <div className="form-block__input-wrapper">
-                <div className="form-group form-group--login">
-                    <Input type="text" id="username" label="user name" disabled={this.props.mode === 'signup'}/>
-                    <Input type="password" id="password" label="password" disabled={this.props.mode === 'signup'}/>
-                </div>
-                <div className="form-group form-group--signup">
-                    <Input type="text" id="fullname" label="full name" disabled={this.props.mode === 'login'} />
-                    <Input type="email" id="email" label="email" disabled={this.props.mode === 'login'} />
-                    <Input type="password" id="createpassword" label="password" disabled={this.props.mode === 'login'} />
-                    <Input type="password" id="repeatpassword" label="repeat password" disabled={this.props.mode === 'login'} />
-                </div>
-            </div>
-            <button className="button button--primary full-width" type="submit">{this.props.mode === 'login' ? 'Log In' : 'Sign Up'}</button>
-        </form>
-        )
-    }
-}
-
-const Input = ({ id, type, label, disabled }) => (
-    <input className="form-group__input" type={type} id={id} placeholder={label} disabled={disabled}/>
+const App = props => (
+    <LoginForm />
 );
 
-const App = () => (
-    <div className={`app app--is-${mode}`}>
-        <LoginComponent
-            mode={mode}
-            onSubmit={
-                function() {
-                    console.log('submit');
-                }
-            }
-        />
+class LoginForm extends React.Component{
+    render(){
+        return(
+            <div id="loginform">
+                <h2 id="headerTitle">Login</h2>
+                <Form />
+            </div>
+        )
+    }
+}
+
+const Form = props => (
+    <div>
+        <FormInput description="Email" placeholder="Enter the Email" type="text" />
+        <FormInput description="Password" placeholder="Enter the password" type="password"/>
+
+        <div id="button" class="row">
+            <button>Login</button>
+        </div>
     </div>
 );
 
-ReactDOM.render( <App/>, document.getElementById("app"));
+
+const FormInput = props => (
+    <div class="row">
+        <label>{props.description}</label>
+        <input type={props.type} placeholder={props.placeholder}/>
+    </div>
+);
+
+export default App
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,106 +1,39 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-import { ToastContainer, toast } from "react-toastify";
+import './Login.css'
 
-const Signup = () => {
-  const navigate = useNavigate();
-  const [inputValue, setInputValue] = useState({
-    email: "",
-    password: "",
-    username: "",
-  });
-  const { email, password, username } = inputValue;
-  const handleOnChange = (e) => {
-    const { name, value } = e.target;
-    setInputValue({
-      ...inputValue,
-      [name]: value,
-    });
-  };
+const App = props => (
+    <LoginForm />
+);
 
-  const handleError = (err) =>
-    toast.error(err, {
-      position: "bottom-left",
-    });
-  const handleSuccess = (msg) =>
-    toast.success(msg, {
-      position: "bottom-right",
-    });
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const { data } = await axios.post(
-        "http://localhost:4000/signup",
-        {
-          ...inputValue,
-        },
-        { withCredentials: true }
-      );
-      const { success, message } = data;
-      if (success) {
-        handleSuccess(message);
-        setTimeout(() => {
-          navigate("/");
-        }, 1000);
-      } else {
-        handleError(message);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-    setInputValue({
-      ...inputValue,
-      email: "",
-      password: "",
-      username: "",
-    });
-  };
-
-  return (
-    <div className="form_container">
-    <div className="text">Sign Up </div>
-    <div className="underline"></div>
-      <form onSubmit={handleSubmit}>
-      <div>
-          <label htmlFor="email">Username</label>
-          <input
-            type="text"
-            name="username"
-            value={username}
-            placeholder="Enter your username"
-            onChange={handleOnChange}
-          />
+class LoginForm extends React.Component{
+  render(){
+    return(
+        <div id="loginform">
+          <h2 id="headerTitle">Signup</h2>
+          <Form />
         </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            placeholder="Enter your email"
-            onChange={handleOnChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            placeholder="Enter your password"
-            onChange={handleOnChange}
-          />
-        </div>
-        <button type="submit">Submit</button>
-        <span>
-          Already have an account? <Link to={"/login"}>Login</Link>
-        </span>
-      </form>
-      <ToastContainer />
+    )
+  }
+}
+
+const Form = props => (
+    <div>
+        <FormInput description="Name" placeholder="Enter the Name" type="text" />
+        <FormInput description="Email" placeholder="Enter the Email" type="text" />
+        <FormInput description="Password" placeholder="Enter the password" type="password"/>
+
+      <div id="button" class="row">
+        <button>Sign Up</button>
+      </div>
     </div>
-  );
-};
+);
 
-export default Signup;
+
+const FormInput = props => (
+    <div class="row">
+      <label>{props.description}</label>
+      <input type={props.type} placeholder={props.placeholder}/>
+    </div>
+);
+
+export default App
