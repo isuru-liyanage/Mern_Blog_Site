f5a7c926f19809f1bde1851ac74be4ea12492300
sineth <dhananjayaaps@gmail.com>
2023-08-27 12:16:07 +0530

    attributes changed

--- server/Models/UserModels.js
+++ server/Models/UserModels.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Your username is required"],
+  },
   email: {
     type: String,
     required: [true, "Your email address is required"],
     unique: true,
-  },
-  username: {
-    type: String,
-    required: [true, "Your username is required"],
   },
   password: {
     type: String,
