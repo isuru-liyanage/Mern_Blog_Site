63920a5452326724028f4b53f03c4fa504878c36
sineth <dhananjayaaps@gmail.com>
2023-08-27 20:59:47 +0530

    sign up connected

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,39 +1,98 @@
 import React, { useState } from "react";
 import './Login.css'
 
-const App = props => (
+const App = () => (
     <LoginForm />
 );
 
-class LoginForm extends React.Component{
-  render(){
-    return(
+const LoginForm = () => {
+    // State to manage form data
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: ""
+    });
+
+    // Function to handle form input changes
+    const handleInputChange = (name, value) => {
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://localhost:4000/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(formData)
+            });
+
+            if (response.ok) {
+                console.log("Form submitted successfully!");
+            } else {
+                console.error("Form submission failed");
+            }
+        } catch (error) {
+            console.error("Network error:", error);
+        }
+    };
+
+
+    return (
         <div id="loginform">
-          <h2 id="headerTitle">Signup</h2>
-          <Form />
+            <h2 id="headerTitle">Signup</h2>
+            <form onSubmit={handleSubmit}>
+                <FormInput
+                    name="name"
+                    description="Name"
+                    placeholder="Enter the Name"
+                    type="text"
+                    value={formData.name}
+                    onChange={handleInputChange}
+                />
+                <FormInput
+                    name="email"
+                    description="Email"
+                    placeholder="Enter the Email"
+                    type="text"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                />
+                <FormInput
+                    name="password"
+                    description="Password"
+                    placeholder="Enter the password"
+                    type="password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                />
+
+                <div id="button" className="row">
+                    <button type="submit">Sign Up</button>
+                </div>
+            </form>
         </div>
-    )
-  }
-}
+    );
+};
 
-const Form = props => (
-    <div>
-        <FormInput description="Name" placeholder="Enter the Name" type="text" />
-        <FormInput description="Email" placeholder="Enter the Email" type="text" />
-        <FormInput description="Password" placeholder="Enter the password" type="password"/>
-
-      <div id="button" class="row">
-        <button>Sign Up</button>
-      </div>
+const FormInput = (props) => (
+    <div className="row">
+        <label>{props.description}</label>
+        <input
+            type={props.type}
+            name={props.name}
+            placeholder={props.placeholder}
+            value={props.value}
+            onChange={(e) => props.onChange(props.name, e.target.value)}
+        />
     </div>
 );
 
 
-const FormInput = props => (
-    <div class="row">
-      <label>{props.description}</label>
-      <input type={props.type} placeholder={props.placeholder}/>
-    </div>
-);
-
-export default App
+export default App;
