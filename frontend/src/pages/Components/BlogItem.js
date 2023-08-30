55152ff1b6c3e7d331763b19c3e7cdd45f6291b1
isuruliyanage2001 <isuruliyanageofficial@gmail.com>
2023-08-30 16:27:05 +0530

    home page updated

--- frontend/src/pages/Components/BlogItem.js
+++ frontend/src/pages/Components/BlogItem.js
@@ -5,11 +5,33 @@ function BlogItem() {
         <div className="item_layout">
             <div className="image_element">
                 <img src="https://www.boardinfinity.com/blog/content/images/2023/01/Mern.png"
-                    loading="lazy" alt="Everything You Need to Know About Scholarships"
+                    loading="lazy"
                     />
             </div>
-            <div className="text_element">
+            <div className="part2">
+                <div className="head">
+                    <h2>Title</h2>
+                    <p>Date and Time</p>
+                </div>
 
+                <div className="description">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis nunc eget lorem dolor sed viverra ipsum. Nisl tincidunt eget nullam non nisi est. Amet venenatis urna cursus eget nunc scelerisque viverra mauris in. Viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Suspendisse faucibus interdum posuere lorem ipsum dolor sit amet. Praesent elementum facilisis leo vel fringilla est. Magna fermentum iaculis eu non diam phasellus vestibulum lorem sed. Laoreet suspendisse interdum consectetur libero. Posuere lorem ipsum dolor sit amet. Amet mattis vulputate enim nulla aliquet porttitor lacus luctus.
+                    </p>
+                </div>
+                <div>
+                    <div id="w-node-b1611847-2652-c590-9994-20784312122a-2f75df94" className="btn--green_240">
+                        <div className="btn-text" >Read More</div>
+                        <div className="btn-icon w-embed" >
+                            <svg width="23" height="20" viewBox="0 0 23 20" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M4.83336 13.3333C6.05558 13.3333 7.05558 14.3333 7.05558 15.5556C7.05558 16.7778 6.05558 17.7778 4.83336 17.7778C3.61114 17.7778 2.61114 16.7778 2.61114 15.5556C2.61114 14.3333 3.61114 13.3333 4.83336 13.3333ZM4.83336 11.1111C2.38892 11.1111 0.388916 13.1111 0.388916 15.5556C0.388916 18 2.38892 20 4.83336 20C7.2778 20 9.2778 18 9.2778 15.5556C9.2778 13.1111 7.2778 11.1111 4.83336 11.1111ZM11.5 2.22222C12.7222 2.22222 13.7222 3.22222 13.7222 4.44444C13.7222 5.66667 12.7222 6.66667 11.5 6.66667C10.2778 6.66667 9.2778 5.66667 9.2778 4.44444C9.2778 3.22222 10.2778 2.22222 11.5 2.22222ZM11.5 0C9.05558 0 7.05558 2 7.05558 4.44444C7.05558 6.88889 9.05558 8.88889 11.5 8.88889C13.9445 8.88889 15.9445 6.88889 15.9445 4.44444C15.9445 2 13.9445 0 11.5 0ZM18.1667 13.3333C19.3889 13.3333 20.3889 14.3333 20.3889 15.5556C20.3889 16.7778 19.3889 17.7778 18.1667 17.7778C16.9445 17.7778 15.9445 16.7778 15.9445 15.5556C15.9445 14.3333 16.9445 13.3333 18.1667 13.3333ZM18.1667 11.1111C15.7222 11.1111 13.7222 13.1111 13.7222 15.5556C13.7222 18 15.7222 20 18.1667 20C20.6111 20 22.6111 18 22.6111 15.5556C22.6111 13.1111 20.6111 11.1111 18.1667 11.1111Z"
+                                    fill="#0F3870"></path>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
 
             </div>
 
