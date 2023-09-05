89bb54a374cb2eb06844de0cb1c3c5e641b43b3d
DilkiSewwandi <2021cs185@stu.ucsc.cmb.ac.lk>
2023-09-05 23:06:53 +0530

    Adding function getSupportsBySupportId

--- server/Controllers/SupportController.js
+++ server/Controllers/SupportController.js
@@ -25,6 +25,67 @@ async function createSupport(req, res) {
 
 }
 
+async function getSupportsBySupportId(req, res) {
+    try {
+        
+        const { SupportId } = req.params;
+        
+        const userId = req.user._id;
+
+        console.log(userId);
+
+        const support = await Support.findOne({ _id: SupportId });
+
+        if( !support ){
+            return res.status(404).json({message : "Invalid Support ID"})
+        }
+
+        if(req.user._id.toString() !== support.userId.toString()){
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+        
+        res.status(200).json({ support });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to support' });
+    }
+
+}
+
+async function DeleteSupport(req, res) {
+    try {
+        
+        const { SupportId } = req.params;
+        
+        const userId = req.user._id;
+
+        console.log(userId);
+
+        const support = await Support.findOne({ _id: SupportId });
+
+        if( !support ){
+            return res.status(404).json({message : "Sopport not found"})
+        }
+       
+
+        if(req.user._id.toString() !== support.userId.toString()){
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+        
+        res.status(200).json({ support });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to support' });
+    }
+
+}
+
+
+
+
 module.exports={
-    createSupport
+    createSupport, getSupportsBySupportId
 };
+
